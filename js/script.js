47cbb18ba32089b984a486043495f0441082d7eb
nazmariam <nazmariam@macpaw.com>
2019-11-22 17:27:31 +0200

    wip

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider-box').slick({
+        dots: true,
+        // infinite: true,
+        slidesToShow: 3,
+        // slidesToScroll: 3,
+        // speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
