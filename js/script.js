dac022e0a54396e6d11e329ef6d4b7b80c9799c1
nazmariam <fedirko.mary@gmail.com>
2019-11-23 17:27:59 +0200

    slick

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
+    // $('.slider-box').slick({
+    //     dots: true,
+    //     // infinite: true,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 3,
+    //     // speed: 500,
+    //     fade: true,
+    //     cssEase: 'linear',
+    //     autoplay: true,
+    //     autoplaySpeed: 2000,
+    // });
     $('.slider-box').slick({
-        dots: true,
-        // infinite: true,
+        infinite: true,
         slidesToShow: 3,
-        // slidesToScroll: 3,
-        // speed: 500,
-        fade: true,
-        cssEase: 'linear',
+        slidesToScroll: 3,
+        dots: true,
         autoplay: true,
         autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
