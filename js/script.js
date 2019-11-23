8c3c84a229ea4cd73490f404a6fb5ec79428c3fe
nazmariam <fedirko.mary@gmail.com>
2019-11-23 17:51:00 +0200

    Slider on tablet + mobile

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
 $(document).ready(function(){
-    // $('.slider-box').slick({
-    //     dots: true,
-    //     // infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3,
-    //     // speed: 500,
-    //     fade: true,
-    //     cssEase: 'linear',
-    //     autoplay: true,
-    //     autoplaySpeed: 2000,
-    // });
+
     $('.slider-box').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.slider-box-mob').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         dots: true,
         autoplay: true,
         autoplaySpeed: 2000,
+        centerMode: true,
         arrows: false,
         responsive: [
             {
